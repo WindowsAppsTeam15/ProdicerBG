dd403ddb3b8c9298587d9cf86459f4d49c3307d9
veselints <veselintsvetanov@gmail.com>
2016-01-17 21:13:20 +0200

    Location more

--- models/producer-model.js
+++ models/producer-model.js
@@ -36,10 +36,10 @@ let producerSchema = new mongoose.Schema({
         match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     addressLongitude: {
-        type: SchemaTypes.Double
+        type: String
     },
     addressLatitude: {
-        type: SchemaTypes.Double
+        type: String
     },
     userId: {
         type: String,
